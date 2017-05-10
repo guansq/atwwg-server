045cd30d92d1ebb8aca9703dc5b7b31751fa696b
William <Wxx@ruitukeji.com>
2017-05-10 18:07:52 +0800

    搭建物供管理控制器

---
+++ application/admin/controller/Chart.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/9
+ * Time: 9:33
+ */
+namespace app\admin\controller;
+
+use controller\BasicAdmin;
+use service\LogService;
+use service\DataService;
+use app\admin\model\AddrModel;
+use think\Db;
+
+class Chart extends BasicAdmin{
+    protected $table = 'SystemArea';
+    protected $title = '相关信息';
+
+    public function index(){
+        //echo '111111111';die;
+        $this->assign('title',$this->title);
+        return view();
+    }
+
+    public function qualified(){
+        $this->assign('title',$this->title);
+        return view();
+    }
+
+    public function time(){
+        $this->assign('title',$this->title);
+        return view();
+    }
+
+}
---
+++ application/admin/controller/Supporter.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/9
+ * Time: 9:33
+ */
+namespace app\admin\controller;
+
+use controller\BasicAdmin;
+use service\LogService;
+use service\DataService;
+use app\admin\model\AddrModel;
+use think\Db;
+
+class Supporter extends BasicAdmin{
+    protected $table = 'SystemArea';
+    protected $title = '相关信息';
+
+    public function index(){
+        //echo '111111111';die;
+        $this->assign('title',$this->title);
+        return view();
+    }
+
+    public function del(){
+
+    }
+
+    public function add(){
+
+    }
+
+}

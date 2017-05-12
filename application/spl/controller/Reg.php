04a335d9622d6bc8a299dca918196b3ff61007d5
William <Wxx@ruitukeji.com>
2017-05-12 18:27:00 +0800

    新增reg

---
+++ application/spl/controller/Reg.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/12
+ * Time: 18:16
+ */
+namespace app\spl\controller;
+use think\Controller;
+use think\Request;
+use think\Db;
+
+class Reg extends Base{
+    /**
+     * 用户登录
+     * @return string
+     */
+    public function index(){
+        return view();
+    }
+}

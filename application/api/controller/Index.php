b2a43a353b1a9adde652eb58e43fb04b6dd509ea
William <Wxx@ruitukeji.com>
2017-05-12 18:04:02 +0800

    短信接口

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -19,6 +19,7 @@ class Index extends Base{
         //dump(config('send_trigger_mail'));
         //dump(config('send_accord_mail'));
         echo $this->send_tmp_mail();
+        echo send_sms();
     }
 
     /**
@@ -129,5 +130,53 @@ class Index extends Base{
 
         return $result;
     }
+
+    function send_sms() {
+        $url = 'http://www.sendcloud.net/smsapi/send';
+
+        $param = array(
+            'smsUser' => '***',
+            'templateId' => '1',
+            'msgType' => '0',
+            'phone' => '13412345678',
+            'vars' => '{"%code%":"123456"}'
+        );
+
+        $sParamStr = "";
+        ksort($param);
+        foreach ($param as $sKey => $sValue) {
+            $sParamStr .= $sKey . '=' . $sValue . '&';
+        }
+
+        $sParamStr = trim($sParamStr, '&');
+        $smskey = '***';
+        $sSignature = md5($smskey."&".$sParamStr."&".$smskey);
+
+
+        $param = array(
+            'smsUser' => '***',
+            'templateId' => '1',
+            'msgType' => '0',
+            'phone' => '13412345678',
+            'vars' => '{"%code%":"123456"}',
+            'signature' => $sSignature
+        );
+
+        $data = http_build_query($param);
+        echo $data;
+
+        $options = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-Type:application/x-www-form-urlencoded',
+                'content' => $data
+
+            ));
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, FILE_TEXT, $context);
+
+        return $result;
+    }
+
 
 }

6ed031ee21849b7e1e36aac976c2a4eb5a6f9549
William <Wxx@ruitukeji.com>
2017-05-22 17:34:51 +0800

    供应商列表

--- application/admin/controller/Supporter.php
+++ application/admin/controller/Supporter.php
@@ -61,8 +61,14 @@ class Supporter extends BaseController{
         return json(['code'=>200,'msg'=>'更新成功！']);
     }
 
-    public function read(){
-        echo 'a11';
+    /**
+     * 得到供应商信息
+     */
+
+    public function getSupList(){
+        $logicSupInfo = Model('Supporter','logic');
+        $list = $logicSupInfo->getListInfo();
+        return $list;
     }
     public function del(){
 

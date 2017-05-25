5f78a334c0fb105238469424f26f7af26306248a
will <314112362@qq.com>
2017-05-25 18:21:56 +0800

    update:询价单列表

--- public/static/admin/enquiryorder/index.js
+++ public/static/admin/enquiryorder/index.js
@@ -3,13 +3,22 @@
  * Created by Administrator on 2017/5/11.
  */
 
-require(["laydate"],function(){
+require(["laydate",'jquery.dataTables',"dataTables.bootstrap"],function(){
   $(document).ready(function(){
     initPage();
   })
 });
 
+
 function initPage(){
+
+  initTimePicker();
+
+  initTable();
+
+}
+
+function initTimePicker(){
   $(".date_time").focus(function(){
     /*
      laydate插件提供显示日期控件的方法
@@ -37,12 +46,54 @@ function initPage(){
   });
 }
 
+function initTable(){
+  var table = $('#example').DataTable({
+    //paging: false, 设置是否分页
+    "info": false,  //去除左下角的信息
+    "lengthChange": false, //是否允许用户改变表格每页显示的记录数
+    "ordering": false, //是否允许Datatables开启排序
+    "searching": false,  //是否允许Datatables开启本地搜索
+    language: {
+      "oPaginate": {
+        "sFirst": "首页",
+        "sPrevious": "上页",
+        "sNext": "下页",
+        "sLast": "末页"
+      }
+    },
+    "processing": true,
+    "serverSide": true,
+    "ajax": {
+      "url": '/Enquiryorder/getInquiryList',
+      "type": "GET"
+    },
+    "pageLength": 20,
+    "columns": [
+      { "data": "io_code" },  //  <th>询价单号</th>
+      { "data": "pr_code"},   //  <th>请购单号</th> },
+      { "data": "item_code" },     //  <th>料号</th>
+      //{ "data": "desc" },       //  <th>物料描述</th>
+      //{ "data": "pro_no" },//  <th>项目号</th>
+      { "data": "tc_uom" },//  <th>交易单位</th>,
+      { "data": "price_uom" },//  <th>计价单位</th>
+      { "data": "price_num" },//  <th>数量</th>
+      { "data": "req_date" },//  <th>交期</th>
+      { "data": "quote_date" },//  <th>询价日期</th>
+      { "data": "quote_endtime" },//  <th>报价截止日期</th>
+
+      { "data": "price_status" },   //  <th>报价状态</th>
+      { "data": "status" },         //  <th>状态</th>
+      { "data": "pur_attr" }        //  <th>详情</th>
+    ]
+  });
+}
+
 //待审批弹框点击事件
 $(".approve").click(function(){
   $(".barcode_box").css("display","block");
-})
+});
 
 //关闭弹框
 $(".close_box").click(function(){
   $(".barcode_box").css("display","none");
-})
+});

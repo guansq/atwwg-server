2c7f8b2dcdbeea17e499c334c57a75e574d20214
will <314112362@qq.com>
2017-05-31 10:38:33 +0800

    update:arv_goods_num

--- application/spl/logic/Order.php
+++ application/spl/logic/Order.php
@@ -13,7 +13,7 @@ use app\common\model\Po;
 class Order extends BaseLogic{
 
     function getOrderListInfo(){
-        $list = Po::alias('a')->field('b.po_code,a.order_code,a.status,b.arr_goods_num,b.pro_goods_num,a.contract_time')->join('po_item b','a.order_code= b.po_code')->select();
+        $list = Po::alias('a')->field('b.po_code,a.order_code,a.status,b.arv_goods_num,b.pro_goods_num,a.contract_time')->join('po_item b','a.order_code= b.po_code')->select();
         //echo $this->getLastSql();//die;
         if($list){
             $list = collection($list)->toArray();

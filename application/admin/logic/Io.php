fffd9589f2ab53d5a72b64e3ce4007177366a82b
William <Wxx@ruitukeji.com>
2017-06-01 10:26:01 +0800

    fix bug

--- application/admin/logic/Io.php
+++ application/admin/logic/Io.php
@@ -37,7 +37,7 @@ class Io extends BaseLogic{
      }
 
      function getIoInfo($io_code){
-         $list = IoModel::alias('a')->field('a.id,a.sup_name,a.quote_price,a.quote_date,b.ctc_name,b.mobile,b.email')->join('supplier_info b','a.sup_code=b.code')->where('io_code',"$io_code")->select();
+         $list = IoModel::alias('a')->field('a.id,a.sup_code,a.sup_name,a.quote_price,a.quote_date,b.ctc_name,b.mobile,b.email')->join('supplier_info b','a.sup_code=b.code')->where('io_code',"$io_code")->select();
          if($list){
              $list = collection($list)->toArray();
          }

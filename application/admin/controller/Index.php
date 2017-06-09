f05a765fc6f21d14908818f42b4d9658ff7dbef4
William <Wxx@ruitukeji.com>
2017-06-09 16:26:50 +0800

    fix bug

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -16,6 +16,7 @@ namespace app\admin\controller;
 
 use app\admin\logic\SystemNode;
 use service\DataService;
+use service\NodeService;
 use service\ToolsService;
 use think\Db;
 use think\View;
@@ -34,10 +35,36 @@ class Index extends BaseController {
      * @return View
      */
     public function index() {
+        NodeService::applyAuthNode();
+        $list = Db::name('SystemMenu')->where('status', '1')->order('sort asc,id asc')->select();
+        $menus = $this->_filterMenu(ToolsService::arr2tree($list));
         $this->assign('title', '系统管理');
+        $this->assign('menus', $menus);
         return view();
     }
 
+    /**
+     * 后台主菜单权限过滤
+     * @param array $menus
+     * @return array
+     */
+    private function _filterMenu($menus) {
+        foreach ($menus as $key => &$menu) {
+            if (!empty($menu['sub'])) {
+                $menu['sub'] = $this->_filterMenu($menu['sub']);
+            }
+            if (!empty($menu['sub'])) {
+                $menu['url'] = '#';
+            } elseif (stripos($menu['url'], 'http') === 0) {
+                continue;
+            } elseif ($menu['url'] !== '#' && auth(join('/', array_slice(explode('/', $menu['url']), 0, 3)))) {
+                $menu['url'] = url($menu['url']);
+            } else {
+                unset($menus[$key]);
+            }
+        }
+        return $menus;
+    }
 
 
     /**

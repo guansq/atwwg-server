a82315c25c14fa1527c86006c9e7e24440a2fb6b
will <314112362@qq.com>
2017-06-09 16:50:55 +0800

    fix:中标状态

--- application/admin/logic/Io.php
+++ application/admin/logic/Io.php
@@ -5,39 +5,31 @@
  * Date: 2017/5/24
  * Time: 9:57
  */
+
 namespace app\admin\logic;
 
 use app\common\model\Io as IoModel;
 
 class Io extends BaseLogic{
 
-     function getIoList($start,$length,$where){
-        //->join('u9_pr c','a.pr_code=c.pr_code','LEFT'),c.pro_no
-         $list = IoModel::alias('a')
-             ->field('a.*,b.desc,pr.pro_no,pr.status as pr_status')
-             ->join('item b','a.item_code=b.code','LEFT')
-             ->join('u9_pr pr','pr.id = a.pr_id','LEFT')
-             ->limit("$start,$length")
-             ->group('pr_id');
-         if(!empty($where)){
-             $list = $list->where($where);
-         }
-        $list = $list->select();
-        if($list){
-            $list = collection($list)->toArray();
-        }
-        return $list;
-     }
 
-     /*
-      * 得到不含分页的数据
-      */
-    function getIoAllList($where){
+    const STATUS_ARR = [
+        'init' => '未报价',
+        'quoted' => '已报价',
+        'winbid_uncheck' => '中标但是需要审核',
+        'winbid_checked' => '中标已经审核',
+        'winbid' => '中标',
+        'giveupbid' => '弃标',
+        'close' => '已关闭',
+    ];
+
+    function getIoList($start, $length, $where){
         //->join('u9_pr c','a.pr_code=c.pr_code','LEFT'),c.pro_no
         $list = IoModel::alias('a')
             ->field('a.*,b.desc,pr.pro_no,pr.status as pr_status')
-            ->join('item b','a.item_code=b.code','LEFT')
-            ->join('u9_pr pr','pr.id = a.pr_id','LEFT')
+            ->join('item b', 'a.item_code=b.code', 'LEFT')
+            ->join('u9_pr pr', 'pr.id = a.pr_id', 'LEFT')
+            ->limit("$start,$length")
             ->group('pr_id');
         if(!empty($where)){
             $list = $list->where($where);
@@ -49,66 +41,96 @@ class Io extends BaseLogic{
         return $list;
     }
 
-     function getListNum($where){
-         $count = IoModel::alias('a')
-             ->field('a.id')
-             ->join('item b','a.item_code=b.code','LEFT')
-             ->join('u9_pr pr','pr.id = a.pr_id','LEFT')
-             ->group('pr_id');
-         if(!empty($where)){
-             $count = $count->where($where);
+    /*
+     * 得到不含分页的数据
+     */
+    function getIoAllList($where){
+        //->join('u9_pr c','a.pr_code=c.pr_code','LEFT'),c.pro_no
+        $list = IoModel::alias('a')
+            ->field('a.*,b.desc,pr.pro_no,pr.status as pr_status')
+            ->join('item b', 'a.item_code=b.code', 'LEFT')
+            ->join('u9_pr pr', 'pr.id = a.pr_id', 'LEFT')
+            ->group('pr_id');
+        if(!empty($where)){
+            $list = $list->where($where);
+        }
+        $list = $list->select();
+        if($list){
+            $list = collection($list)->toArray();
+        }
+        return $list;
+    }
+
+    function getListNum($where){
+        $count = IoModel::alias('a')
+            ->field('a.id')
+            ->join('item b', 'a.item_code=b.code', 'LEFT')
+            ->join('u9_pr pr', 'pr.id = a.pr_id', 'LEFT')
+            ->group('pr_id');
+        if(!empty($where)){
+            $count = $count->where($where);
+        }
+        $count = $count->select();
+        //$count = IoModel::alias('a')->field('a.*,b.desc')->join('item b','a.item_code=b.code','LEFT')->group('pr_code,item_code')->count();
+        return $count;
+    }
+
+    function getIoCountByWhere($where){
+        $count = IoModel::where($where)->count();
+        return $count;
+    }
+
+    function getIoInfo($io_code){
+        $list = IoModel::alias('a')
+            ->field('a.*,b.ctc_name,b.mobile,b.email')
+            ->join('supplier_info b', 'a.sup_code=b.code', 'LEFT')
+            ->where('io_code', "$io_code")
+            ->select();
+        //->join('u9_pr c','a.pr_code=c.pr_code','LEFT'),c.pro_no
+        //echo $this->getLastSql();
+        if($list){
+            $list = collection($list)->toArray();
+        }
+
+        foreach($list as &$item){
+            $item['statusStr'] = self::STATUS_ARR[$item['status']];
          }
-         $count = $count->select();
-         //$count = IoModel::alias('a')->field('a.*,b.desc')->join('item b','a.item_code=b.code','LEFT')->group('pr_code,item_code')->count();
-         return $count;
-     }
+        return $list;
+    }
 
-     function getIoCountByWhere($where){
-         $count = IoModel::where($where)->count();
-         return $count;
-     }
+    /*
+     * 得到询价待审批数量
+     */
+    function getQuoteNum(){
+        return IoModel::where('status', 'in', ['quoted'])->count();
+    }
 
-     function getIoInfo($io_code){
-         $list = IoModel::alias('a')->field('a.*,b.ctc_name,b.mobile,b.email')
-                ->join('supplier_info b','a.sup_code=b.code','LEFT')
-                ->where('io_code',"$io_code")->select();
-         //->join('u9_pr c','a.pr_code=c.pr_code','LEFT'),c.pro_no
-         //echo $this->getLastSql();
-         if($list){
-             $list = collection($list)->toArray();
-         }
-         return $list;
-     }
+    /*
+     *得到流拍询价数量
+     */
+    function getGiveupNum(){
+        return IoModel::where('status', 'in', ['giveupbid'])->count();
+    }
 
-     /*
-      * 得到询价待审批数量
-      */
-     function getQuoteNum(){
-         return IoModel::where('status','in',['quoted'])->count();
-     }
+    /*
+     * 通过条件得到询价单数量
+     */
+    function getIoNumByWhere($where){
+        return IoModel::where($where)->count();
+    }
 
-     /*
-      *得到流拍询价数量
-      */
-     function getGiveupNum(){
-         return IoModel::where('status','in',['giveupbid'])->count();
-     }
-
-     /*
-      * 通过条件得到询价单数量
-      */
-     function getIoNumByWhere($where){
-         return IoModel::where($where)->count();
-     }
-
-     /*
-      * 获取supid通过ioid
-      */
-     function getSupId($where){
-         $list = IoModel::alias('a')->field('b.sup_id,b.phone,b.email')->join('supplier_info b','a.sup_code=b.code','LEFT')->where($where)->find();
-         if($list){
-             $list = $list->toArray();
-         }
-         return $list;
-     }
+    /*
+     * 获取supid通过ioid
+     */
+    function getSupId($where){
+        $list = IoModel::alias('a')
+            ->field('b.sup_id,b.phone,b.email')
+            ->join('supplier_info b', 'a.sup_code=b.code', 'LEFT')
+            ->where($where)
+            ->find();
+        if($list){
+            $list = $list->toArray();
+        }
+        return $list;
+    }
 }

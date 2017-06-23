42bd0c938225965cc7fb7ad3b8647fbeb9889df0
will <314112362@qq.com>
2017-06-23 19:03:31 +0800

    fix: credit_total

--- application/admin/logic/Item.php
+++ application/admin/logic/Item.php
@@ -5,6 +5,7 @@
  * Date: 2017/5/23
  * Time: 10:00
  */
+
 namespace app\admin\logic;
 
 use app\common\model\Item as ItemModel;
@@ -15,7 +16,7 @@ class Item extends Baselogic{
      * 判断U9的数据是否存在
      */
     public function exist($data){
-        $count =ItemModel::field('code')->where('code',$data['code'])->count();
+        $count = ItemModel::field('code')->where('code', $data['code'])->count();
         return $count == 0 ? true : false;//不存在true 存在false
     }
 
@@ -29,13 +30,13 @@ class Item extends Baselogic{
     /*
      * 得到U9供应商数据分页
      */
-    public function getListInfo($start,$length,$where = []){
+    public function getListInfo($start, $length, $where = []){
         if(empty($where)){
             $list = ItemModel::limit("$start,$length")->order('update_at desc')->select();
         }else{
             $list = ItemModel::where($where)->order('update_at desc')->limit("$start,$length")->select();
         }
-        if($list) {
+        if($list){
             $list = collection($list)->toArray();
         }
         //dump($list);die;
@@ -52,12 +53,13 @@ class Item extends Baselogic{
             $list = ItemModel::select();
         }
 
-        if($list) {
+        if($list){
             $list = collection($list)->toArray();
         }
         //dump($list);die;
         return $list;
     }
+
     /*
      * 得到U9供应商数据总数
      */
@@ -75,7 +77,12 @@ class Item extends Baselogic{
      * 得到物料关联的供应商
      */
     public function getRelationSup($item_code){
-        $list = model('U9SupItem')->alias('a')->field('a.*,b.tech_score,b.risk_level,b.risk_level')->join('supplier_info b','a.sup_code = b.code','LEFT')->where("item_code","$item_code")->select();
+        $list = model('U9SupItem')
+            ->alias('a')
+            ->field('a.*,b.tech_score,b.risk_level,b.risk_level,b.credit_total')
+            ->join('supplier_info b', 'a.sup_code = b.code', 'LEFT')
+            ->where("item_code", "$item_code")
+            ->select();
         //echo $this->getLastSql();
         if($list){
             //echo '111111';
@@ -88,8 +95,8 @@ class Item extends Baselogic{
      *料号信息
      */
     public function getItemInfo($code){
-        $list = ItemModel::where("code","$code")->find();
-        if($list) {
+        $list = ItemModel::where("code", "$code")->find();
+        if($list){
             $list = $list->toArray();
         }
         return $list;
@@ -99,14 +106,14 @@ class Item extends Baselogic{
     /*
      * 编辑物料详情
      */
-    function updateByCode($code,$data){
-        return ItemModel::where(['code'=>$code])->update($data);
+    function updateByCode($code, $data){
+        return ItemModel::where(['code' => $code])->update($data);
     }
 
     /*
      * 更新item
      */
-    public function saveItem($where,$data){
+    public function saveItem($where, $data){
         return ItemModel::where($where)->update($data);
     }
 
@@ -114,7 +121,7 @@ class Item extends Baselogic{
      * 自增 update_cnt 更新次数
      */
     public function setIncUpdate($code){
-        return ItemModel::where(['code'=>$code])->setInc('update_cnt');
+        return ItemModel::where(['code' => $code])->setInc('update_cnt');
     }
 
 }

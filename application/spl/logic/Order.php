83dbc4ec8b6deef955e646cf1af0b16ce98dba43
will <314112362@qq.com>
2017-06-23 11:09:11 +0800

    fix: 更改下载名称

--- application/spl/logic/Order.php
+++ application/spl/logic/Order.php
@@ -436,7 +436,7 @@ EOD;
 
         // Close and output PDF document
         // This method has several options, check the source code documentation for more information.
-        $pdf->Output('example_001.pdf', 'D');
+        $pdf->Output("$po[order_code].pdf", 'D');
         exit();
 
     }
